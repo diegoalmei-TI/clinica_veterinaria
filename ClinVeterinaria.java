5ef3af78d19e26f61ec8ece9b8cdc98b4a20dd12
Diego.Almeida.Silva <diego.a.d.silva958@gmail.com>
2026-08-27 11:24:10 -0300

    Add files via upload

---
+++ ClinVeterinaria.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clinveterinaria;
+
+/**
+ *
+ * @author diego
+ */
+public class ClinVeterinaria {
+
+    public static void main(String[] args) {
+        FormularioPrincipal f = new FormularioPrincipal();
+        f.setVisible(true);
+    }
+}
